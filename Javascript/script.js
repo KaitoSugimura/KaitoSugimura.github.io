91633119033040b87d89d5a1579df8e6fd464307
Kaito Sugimura <sugimurakaito@gmail.com>
2021-07-31 15:33:03 -0600

    Add TextBorderButton to Main banner

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,36 +1,41 @@
 let PhotoIndex = 0;
+let BorderSwitch = false;
 let rightButton = document.getElementById('rightButton');
 let leftButton = document.getElementById('leftButton');
 let bannerImg = document.getElementById('banner');
 let title = document.getElementById('title')
+let BorderButton = document.getElementById('borderButton')
 
 function PhotoRotate () {
     if (PhotoIndex === 0) {
         bannerImg.style.backgroundImage = "url('Photos/KaitoCliff.jpg')";
         title.style.color = '#0fb0f0'
-        title.style.top = '3.2rem'
-        title.style.backgroundColor = 'rgba(0, 0, 0, 0)'
-        title.style.padding = '0.25rem'
+        title.style.top = '4.2rem'
     }
     else if (PhotoIndex === 1) {
         bannerImg.style.backgroundImage = "url('Photos/Karate.jpg')";
         title.style.color = '#ffffff'
-        title.style.top = '3.2rem'
-        title.style.backgroundColor = 'rgba(0, 0, 0, 0)'
-        title.style.padding = '0.25rem'
+        title.style.top = '4.2rem'
     }
     else if (PhotoIndex === 2) {
         bannerImg.style.backgroundImage = "url('Photos/KarateWin.jpg')";
         title.style.color = 'rgb(255, 214, 51)'
         title.style.top = '21rem'
-        title.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
-        title.style.padding = '1rem'
-        title.style.width = '75%'
-        title.style.margin = '0 auto'
-        title.style.borderRadius = '0.5rem'
-    
     }
 } 
+
+function Border () {
+    if (BorderSwitch===false) {
+        title.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+        BorderButton.innerHTML = 'Text Border Off'
+        BorderSwitch = true
+    } else {
+        title.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+        BorderButton.innerHTML = 'Text Border On'
+        BorderSwitch = false
+    }
+}
+
 
 function rightButtonClick () {
     PhotoIndex += 1; 
@@ -50,3 +55,4 @@ function leftButtonClick () {
 
 rightButton.addEventListener('click', rightButtonClick);
 leftButton.addEventListener('click', leftButtonClick);
+BorderButton.addEventListener('click', Border);
