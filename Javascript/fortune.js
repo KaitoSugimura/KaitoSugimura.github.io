7083e3bfff0118cda60c87223164d41f69a128d8
Kaito Sugimura <sugimurakaito@gmail.com>
2021-08-01 22:45:05 -0600

    Finish implementing basic fortune code

--- Javascript/fortune.js
+++ Javascript/fortune.js
@@ -1,3 +1,36 @@
+/* FORTUNE CODE */
+Fortunes = ['Daikichi', 'Chukichi', 'Shokichi', 'kyo', 'daikyo']
+Colors = ['Yellow', 'Green', 'Blue', 'Purple', 'Red', 'Orange', 'Pink', 'White', 'Black']
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+end = ''
+const Fort = {
+    TodaysFortune(name, array) {
+        index = Math.floor(Math.random()*array.length)
+        Fort[name] = array[index]
+    },
+    print(){
+        for (const Fo in Fort){
+            if (typeof(Fort[Fo]) != 'function') {
+                end += `<br><br> Your lucky ${Fo} Today is ${Fort[Fo]} <br>`
+            }
+        }
+    }
+}
+
+function CF(name, ar){
+    Fort.TodaysFortune(name, ar)
+}
+
+function print(){
+    Fort.print()
+}
+
+
+CF('Fortune', Fortunes)
+CF('Color', Colors)
+CF('Number', number)
+print()
+
 /* SOUND VARIABLES */
 const start = new Audio('Audio/Openning.mp3')
 const mystery = new Audio('Audio/Mystery.mp3')
@@ -21,7 +54,7 @@ function buttonPress () {
 function removeCover () {
     OmiSound1.play()
     kuji.style.display = 'inline-block'
-
+    kuji.innerHTML = end
 }
 
 /* MAIN */
@@ -29,38 +62,3 @@ start.play()
 FateButton.addEventListener('click', buttonPress)
 cover.addEventListener('click', removeCover)
 
-
-/* FORTUNE CODE */
-Fortunes = ['Daikichi', 'Chukichi', 'Shokichi', 'kyo', 'daikyo']
-Colors = ['Yellow', 'Green', 'Blue', 'Purple', 'Red', 'Orange', 'Pink', 'White', 'Black']
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-const Fort = {
-    TodaysFortune(name, array) {
-        index = Math.floor(Math.random()*array.length)
-        Fort[name] = array[index]
-    },
-    print(){
-        for (const Fo in Fort){
-            if (typeof(Fort[Fo]) != 'function') {
-                console.log(`Your lucky ${Fo} Today is >>${Fort[Fo]}<<`)
-            }
-        }
-    }
-}
-
-function CF(name, ar){
-    Fort.TodaysFortune(name, ar)
-}
-
-function print(){
-    Fort.print()
-}
-
-
-CF('Fortune', Fortunes)
-CF('Color', Colors)
-CF('Number', number)
-//Update your fortune name here
-CF('MAKEYOUROWNFORTUNE', MAKEYOUROWNFORTUNE)
-print() //This function simply prints all 
